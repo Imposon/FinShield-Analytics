5e7ecc571cb29a6ac65a8b8ae7574ea33e447ff3
Imposon <8461000993as@gmail.com>
2026-03-14 04:46:09 +0530

    fix: service deps

---
+++ backend/src/utils/Logger.ts
@@ -0,0 +1,9 @@
+export class Logger {
+    private static instance: Logger;
+    private constructor() {}
+    public static getInstance(): Logger {
+        if (!Logger.instance) Logger.instance = new Logger();
+        return Logger.instance;
+    }
+    public info(m: string): void { console.log(`[INFO] ${m}`); }
+}
